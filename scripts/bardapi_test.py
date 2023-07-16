2a4d940d3d607c5eeac3ea67db99cd83337dccb1
RektPunk <rektpunk@gmail.com>
2023-07-16 21:09:51 +0900

    feat - add scripts

---
+++ scripts/bardapi_test.py
@@ -0,0 +1,5 @@
+## https://github.com/dsdanielpark/Bard-API
+# from bardapi import Bard
+# token = 'xxxxxxx'
+# bard = Bard(token=token)
+# bard.get_answer("나와 내 동년배들이 좋아하는 뉴진스에 대해서 알려줘")['content']
